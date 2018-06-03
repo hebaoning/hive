ada7986696328d8c16f09d84a07b22ddba51506d
hebaoning <baoning.he@ecreditpal.com>
2018-06-03 16:57:33 +0800

    Function to_char support string date value

--- hplsql/src/main/java/org/apache/hive/hplsql/functions/FunctionString.java
+++ hplsql/src/main/java/org/apache/hive/hplsql/functions/FunctionString.java
@@ -295,17 +295,12 @@ public class FunctionString extends Function {
     }
 
     if (cnt == 2) {
-      Var v1 = evalPop(ctx.func_param(0).expr());
-      if (v1.type == Var.Type.TIMESTAMP || v1.type == Var.Type.DATE) {
-        String sqlFormat = evalPop(ctx.func_param(1).expr()).toString();
-        String format = Utils.convertSqlDatetimeFormat(sqlFormat);
-        if (v1.type == Var.Type.TIMESTAMP) {
-          evalString(new SimpleDateFormat(format).format(v1.timestampValue()));
-        } else {
-          evalString(new SimpleDateFormat(format).format(v1.dateValue()));
-        }
-        return;
-      }
+      Var vt = new Var(Var.Type.TIMESTAMP);
+      vt.cast(evalPop(ctx.func_param(0).expr()));
+      String sqlFormat = evalPop(ctx.func_param(1).expr()).toString();
+      String format = Utils.convertSqlDatetimeFormat(sqlFormat);
+      evalString(new SimpleDateFormat(format).format(vt.timestampValue()));
+      return;
     }
 
     evalNull();
