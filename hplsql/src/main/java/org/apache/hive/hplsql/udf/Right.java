cdb920f7db283ef9c361dfa3fb7e1fd22eae531a
hebaoning <baoning.he@ecreditpal.com>
2018-10-19 17:49:34 +0800

    Fix UDF left/right

--- hplsql/src/main/java/org/apache/hive/hplsql/udf/Right.java
+++ hplsql/src/main/java/org/apache/hive/hplsql/udf/Right.java
@@ -2,36 +2,42 @@ package org.apache.hive.hplsql.udf;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
-import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.UDFType;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters.Converter;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector.PrimitiveCategory;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.IntObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
 import org.apache.hadoop.io.Text;
+
+import static org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils.PrimitiveGrouping.NUMERIC_GROUP;
+import static org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils.PrimitiveGrouping.STRING_GROUP;
+import static org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils.PrimitiveGrouping.VOID_GROUP;
 
 @UDFType(deterministic = true)
 public class Right extends GenericUDF {
 
   private ObjectInspector[] argumentsOI;
+  private transient Converter[] converters = new Converter[1];
+  private transient PrimitiveCategory[] inputTypes = new PrimitiveCategory[1];
 
   @Override
   public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
-    if (arguments.length != 2) {
-      throw new UDFArgumentLengthException("At least two arguments");
-    }
-    if (!(arguments[0] instanceof StringObjectInspector)) {
-      throw new UDFArgumentException("First argument must be a string");
-    }
+    checkArgsSize(arguments, 2, 2);
+    checkArgGroups(arguments, 0, inputTypes, STRING_GROUP, NUMERIC_GROUP, VOID_GROUP);
+    obtainStringConverter(arguments, 0, inputTypes, converters);
     argumentsOI = arguments;
     return PrimitiveObjectInspectorFactory.writableStringObjectInspector;
   }
 
   @Override
   public Object evaluate(DeferredObject[] arguments) throws HiveException {
-    String str = ((StringObjectInspector)argumentsOI[0]).getPrimitiveJavaObject(arguments[0].get());
+    if (arguments[0].get() == null || arguments[1].get() == null) {
+      return null;
+    }
+    String str = getStringValue(arguments, 0, converters);
     Integer len = (Integer)((IntObjectInspector)argumentsOI[1]).getPrimitiveJavaObject(arguments[1].get());
     return new Text(StringUtils.right(str, len));
   }
